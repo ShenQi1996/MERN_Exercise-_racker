ffba34456ba592fd1648c12fe69af0973df31160
ShenQi1996 <qisam1989@gmail.com>
2021-07-12 17:19:27 -0400

    done

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 export default class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-dark bg-dark navbar=expand-lg">
+      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
         <Link to="/" className="navbar-brand">
-          Exercise Tracker
+          ExcerTracker
         </Link>
         <div className="collpase navbar-collapse">
           <ul className="navbar-nav mr-auto">
@@ -18,6 +18,11 @@ export default class Navbar extends Component {
             <li className="navbar-item">
               <Link to="/create" className="nav-link">
                 Create Exercise Log
+              </Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/user" className="nav-link">
+                Create User
               </Link>
             </li>
           </ul>

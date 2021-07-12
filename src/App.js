4b16f903c4bef55652749c8e815a172a5f3befbd
ShenQi1996 <qisam1989@gmail.com>
2021-07-12 18:08:15 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
 import Navbar from "./components/navbar.component";
 import ExercisesList from "./components/exercises-list.component";
 import EditExercise from "./components/edit-exercises.component";

7d37fb6f81ef4d3a42a26cafd5c690d7aa0a435c
ShenQi1996 <qisam1989@gmail.com>
2021-07-13 21:07:33 -0400

    push up to heroku

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const mogoose = require("mongoose");
 
+const path = require("path");
+
 require("dotenv").config();
 
 const app = express();
@@ -10,10 +12,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+// const uri = process.env.ATLAS_URI;
 
 mogoose
-  .connect(uri, {
+  .connect(process.env.ATLAS_URI, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -24,6 +26,13 @@ const connection = mogoose.connection;
 connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 //   set up the routes
 const exercisesRouter = require("./routes/exercises");
